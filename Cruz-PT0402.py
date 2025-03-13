906fc5230fb6952c12315e547c163d7a45480658
Togipi <144612566+Togipi@users.noreply.github.com>
2025-03-13 12:33:21 +0800

    Add files via upload

---
+++ Cruz-PT0402.py
@@ -0,0 +1,143 @@
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+
+    def __str__(self):
+        return f"Token({self.type}, {self.value})"
+
+    def __repr__(self):
+        return self.__str__()
+
+# Token types
+INTEGER, PLUS, MINUS, MUL, DIV, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', 'EOF'
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('Invalid character')
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ""
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+    
+    def get_next_token(self):
+       """Lexical analyzer (also known as scanner or tokenizer)
+       This method is responsible for breaking a sentence apart into tokens.
+       One token at a time. """
+       while self.current_char is not None:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+            
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+                
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+                
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+                
+            if self.current_char == '*':
+                self.advance()
+                return Token(MUL, '*')
+                
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV, '/')
+                
+            self.error()
+            
+       return Token(EOF, None)
+
+class Interpreter:
+    def __init__(self, text):
+        self.lexer = Lexer(text)
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise Exception('Invalid syntax')
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+    
+    def factor(self):
+        token = self.current_token
+        self.eat(INTEGER)
+        return token.value
+
+    def term(self):
+        """term: factor ((MUL | DIV) factor)*"""
+        result = self.factor()
+        
+        while self.current_token.type in (MUL, DIV):
+            token = self.current_token
+            if token.type == MUL:
+                self.eat(MUL)
+                result = result * self.factor()
+            elif token.type == DIV:
+                self.eat(DIV)
+                result = result / self.factor()
+        return result
+        exec(f"print({input()})")
+    
+    def expr(self):
+        """Arithmetic expression parsesr / interpreter. 
+     calc> 14 + 2 * 3 - 6 / 2
+     17
+     
+     expr : term ((PLUS | MINUS) term)*
+     term : factor ((MUL | DIV) factor)*
+     factor : INTEGER
+     """
+        result = self.term()
+        
+        while self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result = result + self.term()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result = result - self.term()
+        
+        return result
+        exec(f"print({input()})")
+
+def main():
+    while True:
+        try:
+            text = input("calc> ")
+            interpreter = Interpreter(text)
+            result = interpreter.expr()
+            print(result)
+        except Exception as e:
+            print(e)
+
+if __name__ == '__main__':
+    main()
